344a3e2695223137e62b96d2224c840bf06e8a6e
Hammam GNF <hammamgonjil@gmail.com>
2025-02-01 16:49:14 +0700

    delete duplicate js

--- resources/views/guru/dashboard.blade.php
+++ resources/views/guru/dashboard.blade.php
@@ -75,5 +75,4 @@
         }
     </script>
     <script async defer src="https://buttons.github.io/buttons.js"></script>
-    <script src="{{ asset('js/argon-dashboard.min.js?v=2.0.4') }}"></script>
 @endsection
